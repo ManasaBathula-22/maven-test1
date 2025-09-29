3942637b5058ca839453ac42c6fc7635881b516c
ManasaBathula-22 <140228953+ManasaBathula-22@users.noreply.github.com>
2025-09-29 13:58:16 +0530

    MANASAB0001 - Created Utility.py with odd/even and manasa() function

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.py
@@ -0,0 +1,9 @@
+def even_or_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+def manasa():
+    return "I am Manasa Bathula"
+print(even_or_odd(22))
+print(manasa())
